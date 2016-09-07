14dc02d478e88e307492158e0f0676039f3d1048
Panayotis Vryonis <vrypan@gmail.com>
2016-09-07 18:12:23 +0300

    update.sh will add context info to commit msg
    Currently, local weather and location (defined by IP)
    will be added automatically to the commit message.

--- update.sh
+++ update.sh
@@ -3,9 +3,16 @@
 git pull
 
 filename=`date "+%Y%m%d"`.md
-echo $filename
 vi $filename
-commit_msg="$filename updated"
+
+CONTEXT1=`curl -s -N http://wttr.in/\?m | sed $'s,\x1b\\[[0-9;]*[a-zA-Z],,g' | head -7`
+# CONTEXT1 holds the current weather and location. 
+# Hat tip to http://unix.stackexchange.com/a/140255/188747 for the sed
+# command to remove ansi escape codes from wttr.in output.
+
+commit_msg="$filename updated.
+
+$CONTEXT1"
 git add $filename
 git commit -m "$commit_msg" $filename && git push
 
